dc57b758080c135b0742ed00208fe1ee38856506
VIC0DER <20243ds066@utez.edu.mx>
2025-05-25 13:37:17 -0600

    Programa terminado

--- U1_TI_03_StudentGrade/src/main/java/org/example/Main.java
+++ U1_TI_03_StudentGrade/src/main/java/org/example/Main.java
@@ -1,17 +1,33 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import Model.Student;
+import java.text.DecimalFormat;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Contador para los ciclos for externos
+        int i;
+        //Creamos un objeto DecimalFormat para formatear el promedio final con 2 decimales
+        DecimalFormat df = new DecimalFormat("#.##");
+        //Creamos un arreglo de 5 estudiantes
+        Student[] students = new Student[5];
+        //Creamos un arreglo de 5 nombres para su asignacion a cada estudiante
+        String[] names = {"Victor", "Damian", "Santiago", "Oscar", "Alejandro"};
+        //Recorremos el arreglo de estudiantes
+        for(i = 0; i < students.length; i++) {
+            students[i] = new Student(); //Se tiene que instanciar el objeto para cada estudiante
+            students[i].setName(names[i]); //Asignamos el nombre del arreglo de nombres
+            students[i].setStudentId("20243ds0" + (i + 1)); //Asignamos una matricula a cada estudiante
+            for (int j = 0; j < 5; j++) { //Creamos otro ciclo for para asignar las calificaciones
+                students[i].setGrades(Math.random() * 10, j); //Asignamos una calificacion aleatoria en escala de 0 a 10
+            }
+        }
+        //Muestreo de datos de cada uno de los estudiantes
+        for(i = 0; i < students.length; i++){
+            System.out.println("---Estudiante " + (i + 1) + "---");
+            System.out.println("Nombre: " + students[i].getName());
+            System.out.println("Promedio: " + df.format(students[i].getAverage()));
+            System.out.println("¿Aprobado? " + (students[i].isApproved() ? "Si" : "No"));
         }
     }
 }
